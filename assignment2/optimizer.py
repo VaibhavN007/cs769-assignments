1547c4b9edfb82863305da91cf9ff5d308d9d5d3
Vaibhav Nitnaware <vaibhavnit007@gmail.com>
2023-10-11 21:12:20 -0500

    update optimizer.py

--- assignment2/optimizer.py
+++ assignment2/optimizer.py
@@ -1,6 +1,7 @@
 from typing import Callable, Iterable, Tuple
 
 import torch
+import math
 from torch.optim import Optimizer
 
 
@@ -26,6 +27,12 @@ class AdamW(Optimizer):
         super().__init__(params, defaults)
 
     def step(self, closure: Callable = None):
+        """
+        Performs a single optimization step.
+
+        Arguments:
+            closure (:obj:`Callable`, `optional`): A closure that reevaluates the model and returns the loss.
+        """
         loss = None
         if closure is not None:
             loss = closure()
@@ -37,42 +44,45 @@ class AdamW(Optimizer):
                 grad = p.grad.data
                 if grad.is_sparse:
                     raise RuntimeError("Adam does not support sparse gradients, please consider SparseAdam instead")
-                
-                # grad          : (30522, hidden_size = 768)
-                # lr            : scalar
-                # betas         : tuple(long, long)
-                # eps           : scalar
-                # weight_decay  : scalar
-                # correct_bias  : scalar
 
-                # State should be stored in this dictionary
                 state = self.state[p]
 
-                # initialize state if empty
+                # State initialization
                 if len(state) == 0:
-                    state['m'] = torch.zeros_like(p.data)
-                    state['v'] = torch.zeros_like(p.data)
-                    state['t'] = torch.zeros([])
+                    state["step"] = 0
+                    # Exponential moving average of gradient values
+                    state["exp_avg"] = torch.zeros_like(p.data)
+                    # Exponential moving average of squared gradient values
+                    state["exp_avg_sq"] = torch.zeros_like(p.data)
 
-                # Access hyperparameters from the `group` dictionary
-                lr, (beta1, beta2), eps = group['lr'], group['betas'], group['eps']
-                
-                # Update first and second moments of the gradients
-                state['m'] = beta1 * state['m'] + (1 - beta1) * grad
-                state['v'] = beta2 * state['v'] + (1 - beta2) * grad**2
-                state['t'] += 1
+                exp_avg, exp_avg_sq = state["exp_avg"], state["exp_avg_sq"]
+                beta1, beta2 = group["betas"]
 
-                # Bias correction
-                # Please note that we are using the "efficient version" given in
-                # https://arxiv.org/abs/1412.6980
-                lr = lr * torch.sqrt(1 - beta2**state['t']) / (1 - beta1**state['t'])
-                
-                # Update parameters
-                p.data = p.data - lr * state['m'] / (torch.sqrt(state['v']) + eps)
+                state["step"] += 1
 
-                # Add weight decay after the main gradient-based updates.
-                # Please note that the learning rate should be incorporated into this update.
-                if group['weight_decay'] != 0.0:
-                    p.data = p.data - lr * group['weight_decay'] * p.data
+                # Decay the first and second moment running average coefficient
+                # In-place operations to update the averages at the same time
+                exp_avg.mul_(beta1).add_(grad, alpha=1.0 - beta1)
+                exp_avg_sq.mul_(beta2).addcmul_(grad, grad, value=1.0 - beta2)
+                denom = exp_avg_sq.sqrt().add_(group["eps"])
+
+                step_size = group["lr"]
+                if group["correct_bias"]:  # No bias correction for Bert
+                    bias_correction1 = 1.0 - beta1 ** state["step"]
+                    bias_correction2 = 1.0 - beta2 ** state["step"]
+                    step_size = step_size * math.sqrt(bias_correction2) / bias_correction1
+
+                p.data.addcdiv_(exp_avg, denom, value=-step_size)
+
+                # Just adding the square of the weights to the loss function is *not*
+                # the correct way of using L2 regularization/weight decay with Adam,
+                # since that will interact with the m and v parameters in strange ways.
+                #
+                # Instead we want to decay the weights in a manner that doesn't interact
+                # with the m/v parameters. This is equivalent to adding the square
+                # of the weights to the loss with plain (non-momentum) SGD.
+                # Add weight decay at the end (fixed version)
+                if group["weight_decay"] > 0.0:
+                    p.data.add_(p.data, alpha=-group["lr"] * group["weight_decay"])
 
         return loss
