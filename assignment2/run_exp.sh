b8af08f8d1b79125180d64e175b30c013b05ea21
Vaibhav Nitnaware <vaibhavnit007@gmail.com>
2023-10-11 21:14:12 -0500

    update run file

--- assignment2/run_exp.sh
+++ assignment2/run_exp.sh
@@ -4,15 +4,30 @@ mkdir -p $CAMPUSID
 
 # Step 1. (Optional) Any preprocessing step, e.g., downloading pre-trained word embeddings
 
-
 # Step 2. Train models on two datasets.
 ##  2.1. Run experiments on SST
-PREF='sst'
-python classifier.py \
+PREF="sst"
+python3 classifier.py \
+    --use_gpu \
+    --option pretrain \
+    --lr 1e-3 \
+    --seed 1234 \
+    --epochs 10 \
+    --train "data/${PREF}-train.txt" \
+    --dev "data/${PREF}-dev.txt" \
+    --test "data/${PREF}-test.txt" \
+    --dev_out "${CAMPUSID}/${PREF}-dev-pre-output.txt" \
+    --test_out "${CAMPUSID}/${PREF}-test-pre-output.txt" \
+    --filepath "${CAMPUSID}/${PREF}-model.pt" | tee ${CAMPUSID}/${PREF}-train-log.txt
+
+
+PREF="sst"
+python3 classifier.py \
     --use_gpu \
     --option finetune \
     --lr 1e-5 \
     --seed 1234 \
+    --epochs 10 \
     --train "data/${PREF}-train.txt" \
     --dev "data/${PREF}-dev.txt" \
     --test "data/${PREF}-test.txt" \
@@ -22,18 +37,31 @@ python classifier.py \
 
 ##  2.2 Run experiments on CF-IMDB
 PREF='cfimdb'
-python classifier.py \
+python3 classifier.py \
+    --use_gpu \
+    --option pretrain \
+    --lr 1e-3 \
+    --seed 1234 \
+    --epochs 10 \
+    --train "data/${PREF}-train.txt" \
+    --dev "data/${PREF}-dev.txt" \
+    --test "data/${PREF}-test.txt" \
+    --dev_out "${CAMPUSID}/${PREF}-dev-pre-output.txt" \
+    --test_out "${CAMPUSID}/${PREF}-test-pre-output.txt" \
+    --filepath "${CAMPUSID}/${PREF}-model.pt" | tee ${CAMPUSID}/${PREF}-train-log.txt
+
+python3 classifier.py \
     --use_gpu \
     --option finetune \
     --lr 1e-5 \
     --seed 1234 \
+    --epochs 10 \
     --train "data/${PREF}-train.txt" \
     --dev "data/${PREF}-dev.txt" \
     --test "data/${PREF}-test.txt" \
     --dev_out "${CAMPUSID}/${PREF}-dev-output.txt" \
     --test_out "${CAMPUSID}/${PREF}-test-output.txt" \
     --filepath "${CAMPUSID}/${PREF}-model.pt" | tee ${CAMPUSID}/${PREF}-train-log.txt
-
 
 
 # Step 3. Prepare submission:
@@ -44,5 +72,5 @@ for file in *.md; do cp $file ${CAMPUSID}/ done
 for file in *.txt; do cp $file ${CAMPUSID}/ done
 
 ##  3.2. Compress the $CAMPUSID folder to $CAMPUSID.zip (containing only .py/.txt/.pdf/.sh files)
-python prepare_submit.py ${CAMPUSID} ${CAMPUSID}
+python3 prepare_submit.py ${CAMPUSID} ${CAMPUSID}
 ##  3.3. Submit the zip file to Canvas! Congrats!
